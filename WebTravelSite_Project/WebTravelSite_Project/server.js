da9c7010745cdc91bdefe2e8f1a9f571486dc3b7
Chris Gawbill <36249471+Ghostbear99@users.noreply.github.com>
2020-11-01 15:35:19 -0500

    Add files via upload

--- WebTravelSite_Project/WebTravelSite_Project/server.js
+++ WebTravelSite_Project/WebTravelSite_Project/server.js
@@ -1,38 +1,49 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
-var url = require("url");
-var fs = require("fs");
-var server = http.createServer(function (req, res) {
-    var path = url.parse(req.url).pathname;
-    switch (path) {
-        case '/':
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<!doctype html>\n<html lang="en">\n' +
-                '\n<meta charset="utf-8">\n<title>Test Web Page</title>\n' +
-                '\n\n<h1>Test</h1>\n' +
-                '<div><p>Here We are <p></div>' +
-                '\n\n');
-            res.end();
-            break;
-        case '/Index.html':
-            fs.readFile(__dirname + path, function (error, data) {
-                if (error) {
-                    res.writeHead(404);
-                    res.write(error);
-                    res.end();
-                } else {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(data);
-                    res.end;
-                }
-            });
-            break;
-        default:
-            res.writeHead(404);
-            res.write("OOOPS -- This page does not exist");
-            res.end();
-            break;
+const http = require('http');
+const port = process.env.PORT || 1337;
+const url = require("url");
+const fs = require("fs");
+let server = http.createServer(function (req, res) {
+    let path = url.parse(req.url).pathname;
+    if (req.url.indexOf('.html') != -1) {
+        fs.readFile(__dirname + '/' + path, function (error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write(error);
+                res.end();
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            }
+        });
     }
+    if (req.url.indexOf('.css') != -1) {
+        fs.readFile(__dirname + '/HomeStyle.css', function (error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write(error);
+                res.end();
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+                res.write(data);
+                res.end();
+            }
+        });
+    }
+    if (req.url.indexOf('.js') != -1) {
+        fs.readFile(__dirname + '/HomeTestScript.js', function (error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write(error);
+                res.end();
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/javascript' });
+                res.write(data);
+                res.end();
+            }
+        });
+    }
+ 
 });
 server.listen(port);
