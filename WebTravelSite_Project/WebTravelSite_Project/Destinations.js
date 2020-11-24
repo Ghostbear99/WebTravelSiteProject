2619e11de961024f942314218b71962067bbbc77
Chris Gawbill <36249471+Ghostbear99@users.noreply.github.com>
2020-11-24 12:26:08 -0500

    Add files via upload

--- WebTravelSite_Project/WebTravelSite_Project/Destinations.js
+++ WebTravelSite_Project/WebTravelSite_Project/Destinations.js
@@ -39,12 +39,23 @@ function createState(stateDoc, stateName) {
     stateDiv.className = "state-tile";
     stateDiv.id = stateName + "-tile";
     stateDiv.value = stateName;
+    
 
     let stateImageDiv = document.createElement("div");
     stateImageDiv.className = "state-img";
 
     let stateImage = document.createElement("img");
-    stateImage.src = "https://philadelphiaencyclopedia.org/wp-content/uploads/2015/09/Philadelphia_Skyline-1.jpg";
+    let request = new XMLHttpRequest();
+    let stateIndex = stateNames.indexOf(stateDiv.value)
+    request.open("GET", "https://localhost:44342/api/Destination/GetState/" + stateIndex);
+    request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            let temp = (request.responseText).split('|');
+            stateImage.src = temp[1];
+        }
+    };
+    request.send();
 
     let stateTextDiv = document.createElement("div");
 
@@ -61,88 +72,6 @@ function createState(stateDoc, stateName) {
     masterDocument.appendChild(stateDiv);
 
     stateDiv.onclick = function () {
-        let request = new XMLHttpRequest();
-        let stateIndex = stateNames.indexOf(stateDiv.value)
-        request.open("GET", "https://localhost:44342/api/Destination/GetState/" + stateIndex);
-        request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-        request.onreadystatechange = function () {
-            if (request.readyState == 4 && request.status == 200) {
-                let temp = (request.responseText).split('|');
-                stateInfo(stateDoc, temp);
-            }
-        };
-        request.send();
+        parent.location = "http://localhost:1337/Bookings.html";
     }
-}
-function clearStateResults() {
-    let masterDoc = document.getElementById("search-state-results");
-    while (masterDoc.hasChildNodes()) {
-        masterDoc.removeChild(masterDoc.firstChild);
-    }
-}
-function stateInfo(stateDoc, infoArray) {
-    let masterDocument = stateDoc;
-    let arr = infoArray;
-
-    masterDocument.style.display = "none";
-
-    let stateDocument = document.getElementById("state-info");
-
-    let header = document.createElement("h1");
-    let headerText = document.createTextNode(arr[0]);
-    header.appendChild(headerText);
-
-    let info = document.createElement("p");
-    let infoText = document.createTextNode(arr[1]);
-    info.appendChild(infoText);
-
-    let locationOneHeader = document.createElement("h2");
-    let locationOneHeaderText = document.createTextNode(arr[2]);
-    locationOneHeader.appendChild(locationOneHeaderText);
-
-    let locationOneImage = document.createElement("img");
-    locationOneImage.src = arr[3];
-
-    let locationOneInfo = document.createElement("p");
-    let locationOneInfoText = document.createTextNode(arr[4]);
-    locationOneInfo.appendChild(locationOneInfoText);
-
-    let locationTwoHeader = document.createElement("h2");
-    let locationTwoHeaderText = document.createTextNode(arr[5]);
-    locationTwoHeader.appendChild(locationTwoHeaderText);
-
-    let locationTwoImage = document.createElement("img");
-    locationTwoImage.src = arr[6];
-
-    let locationTwoInfo = document.createElement("p");
-    let locationTwoInfoText = document.createTextNode(arr[7]);
-    locationTwoInfo.appendChild(locationTwoInfoText);
-    /*
-    let locationThreeHeader = document.createElement("h2");
-    let locationThreeHeaderText = document.createTextNode(arr[8]);
-    locationThreeHeader.appendChild(locationThreeHeaderText);
-
-    let locationThreeImage = document.createElement("img");
-    locationThreeImage.src = arr[9];
-
-    let locationThreeInfo = document.createElement("p");
-    let locationThreeInfoText = document.createTextNode(arr[10]);
-    locationThreeInfo.appendChild(locationThreeInfoText);
-    */
-
-
-    stateDocument.appendChild(header);
-    stateDocument.appendChild(info);
-    stateDocument.appendChild(locationOneHeader);
-    stateDocument.appendChild(locationOneImage);
-    stateDocument.appendChild(locationOneInfoText);
-    stateDocument.appendChild(locationTwoHeader);
-    stateDocument.appendChild(locationTwoImage);
-    stateDocument.appendChild(locationTwoInfoText);
-    /*
-    stateDocument.appendChild(locationThreeHeader);
-    stateDocument.appendChild(locationThreeImage);
-    stateDocument.appendChild(locationThreeInfoText);
-    */
-
 }
