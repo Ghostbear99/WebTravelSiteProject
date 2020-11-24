255d3fd10316447374f9a8b2c9d326f0c486ae65
Chris Gawbill <36249471+Ghostbear99@users.noreply.github.com>
2020-11-24 12:23:59 -0500

    Add files via upload

---
+++ WebTravelSite_Project/DestinationApi/DestinationApi/Models/State.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DestinationApi.Models
+{
+    public class State
+    {
+        private string name;
+        private string imageURL;
+
+        public State(string name, string imageURL)
+        {
+            this.name = name;
+            this.imageURL = imageURL;
+        }
+        public State() { }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Image
+        {
+            get
+            {
+                return imageURL;
+            }
+            set
+            {
+                imageURL = value;
+            }
+        }
+    }
+}
